e92b84d1a3e38334826518ffb638800a52f6b498
nnson1209 <nnson120924@gmail.com>
2024-10-10 15:13:58 +0700

    use semantic and components in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
+    <div>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
 }
